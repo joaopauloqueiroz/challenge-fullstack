f6dbf72e683ff5fcf3aa7b7a9c7fd3189ab62cb1
João Paulo Queiroz <joaopaulo.queiroz.545@gmail.com>
2020-01-15 23:27:46 -0300

    defining model

---
+++ api/src/models/Delivery.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+
+const DeliverySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  weight: {
+    type: Number,
+    required: true
+  },
+  address: {
+    public_place: {
+      type: String,
+      required: true
+    },
+    number: {
+      type: Number,
+      required: true
+    },
+    neighborhood: {
+      type: String,
+      required: true
+    },
+    complement: {
+      type: String
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: String,
+      required: true
+    },
+    country: {
+      type: String,
+      required: true
+    }
+  },
+  geolocation: {
+    latitude: {
+      type: String,
+      required: true
+    },
+    longitude: {
+      type: String,
+      required: true
+    }
+  }
+})
+
+module.exports = model('Delivery', DeliverySchema)
