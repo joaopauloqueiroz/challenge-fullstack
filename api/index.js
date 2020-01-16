a3c0cb723d893bc2f64edc8216f2ed5a8bcadae1
João Paulo Queiroz <joaopaulo.queiroz.545@gmail.com>
2020-01-15 23:07:14 -0300

    creating connection with database

--- api/index.js
+++ api/index.js
@@ -7,6 +7,12 @@ require('dotenv').config();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//connect mongoose
+mongoose.connect(
+  "mongodb+srv://ideas:200*technology@cluster0-jqlte.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true  }
+);
+
 app.listen(process.env.PORT, () => {
-  console.log('Server is running...')
-})
+  console.log('Server is running...');
+});
