b33d4e06de17c1736cef62cec2cc0eb5a9bafc74
João Paulo Queiroz <joaopaulo.queiroz.545@gmail.com>
2020-01-17 14:49:30 -0300

    add const baseURL and key

--- frontend/services/deliveriesService.js
+++ frontend/services/deliveriesService.js
@@ -1,22 +1,24 @@
+const baseURL = 'http://localhost:3333';
+const key = 'AIzaSyCEYIGOcgru2noH5loO938ZKRSGyfvhN78';
 
 app.service('deliveriesService', function ($http) {
   this.listDeliveries = function() {
-    return $http.get('http://localhost:3333/deliveries')
+    return $http.get(`${baseURL}/deliveries`)
   }
 
   this.getAddress = function(address) {
-    return $http.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address},+CA&key=AIzaSyCEYIGOcgru2noH5loO938ZKRSGyfvhN78`)
+    return $http.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address},+CA&key=${key}`)
   }
 
   this.removeAll = function () {
-    return $http.delete('http://localhost:3333/deliveries')
+    return $http.delete(`${baseURL}/deliveries`)
   }
 
   this.removeOne = function (id) {
-    return $http.delete(`http://localhost:3333/deliveries/${id}`)
+    return $http.delete(`${baseURL}/deliveries/${id}`)
   }
 
   this.registerClient = function (data) {
-    return $http.post(`http://localhost:3333/deliveries`, data)
+    return $http.post(`${baseURL}/deliveries`, data)
   }
 })
