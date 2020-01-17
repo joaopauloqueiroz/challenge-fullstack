6683f8ea18174f70767941be424b0fd2b6a918f6
João Paulo Queiroz <joaopaulo.queiroz.545@gmail.com>
2020-01-17 11:32:20 -0300

    clean validators

--- api/src/validator/index.js
+++ api/src/validator/index.js
@@ -2,14 +2,5 @@ const { check } = require('express-validator');
 exports.create = [
   check('name').notEmpty().withMessage('The name field is required'),
   check('weight').notEmpty().withMessage('The weight field is required'),
-  check('address.public_place').notEmpty().withMessage('The public_place field is required'),
-  check('address.street').notEmpty().withMessage('The street field is required'),
-  check('address.number').notEmpty().withMessage('The number field is required'),
-  check('address.number').isNumeric().withMessage('The number field is not a number'),
-  check('address.neighborhood').notEmpty().withMessage('The neighborhood field is required'),
-  check('address.city').notEmpty().withMessage('The city field is required'),
-  check('address.state').notEmpty().withMessage('The state field is required'),
-  check('address.country').notEmpty().withMessage('The country field is required'),
-  check('geolocation.latitude').notEmpty().withMessage('The latitude field is required'),
-  check('geolocation.longitude').notEmpty().withMessage('The longitude field is required'),
+  check('address').notEmpty().withMessage('The field address is required'),
 ]
